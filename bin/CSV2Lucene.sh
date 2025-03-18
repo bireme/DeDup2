d9f5b7fbc4a1306d61a89c579464d7943728c95f
heitorbarbieri <superheitor@gmail.com>
2025-03-18 11:33:14 -0300

    version 1.0.4 java path included in shell files

--- bin/CSV2Lucene.sh
+++ bin/CSV2Lucene.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+JAVA_HOME="/home/users/operacao/.cache/coursier/arc/https/github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.6%252B7/OpenJDK21U-jdk_x64_linux_hotspot_21.0.6_7.tar.gz/jdk-21.0.6+7"
+PATH="$JAVA_HOME/bin:$PATH"
+
 cd /home/javaapps/sbt-projects/DeDup2 || exit
 
+# shellcheck disable=SC2016
 sbt 'runMain dd.tools.CSV2Lucene $1 $2 $3 $4 $5 $6 $7'
 
 cd - || exit
--- bin/SimilarDocs.sh
+++ bin/SimilarDocs.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+JAVA_HOME="/home/users/operacao/.cache/coursier/arc/https/github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.6%252B7/OpenJDK21U-jdk_x64_linux_hotspot_21.0.6_7.tar.gz/jdk-21.0.6+7"
+PATH="$JAVA_HOME/bin:$PATH"
+
 cd /home/javaapps/sbt-projects/DeDup2 || exit
 
 sbt 'runMain dd.SimilarDocs $1 $2 $3 $4 $5 $6 $7 $8 $9'
